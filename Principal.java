a06933d74d9b0b3bb11085d99abff1b340728bab
Franco Valerio <franco.gvp@hotmail.com>
2015-10-15 23:00:22 -0300

    Borrar weas

--- Principal.java
+++ Principal.java
@@ -25,17 +25,6 @@ public class Principal {
 
 		Compania datosEmpresa = null; // Aqui­ se guardaran los datos de la clase empresa
 
-		// Conexión la base de datos de Postgres
-		datosEmpresa = bd.leerDatosBD();
-		if (datosEmpresa != null)
-			// Si se crearon los datos de la empresa
-			System.out.println("Datos leidos desde la base de datos");
-		else {
-			// Si hubo cualquier especie de error al conectar a la BD o al crear los datos.
-			System.err.println("ERROR FATAL: No se obtubieron datos desde la base de datos. ");
-			System.exit(0);
-		}
-
 //================================Llama a FrameInterfaz==========================================
 		FrameInterfaz iPrincipal = new FrameInterfaz(datosEmpresa);
 		iPrincipal.setVisible(true);
